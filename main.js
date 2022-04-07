862922503408f13651106059ba82600b2685328e
adriansan21 <asanchezrodriguez@elpuig.xeill.net>
2022-04-07 10:59:49 +0200

    Diseño Royal Gutters

--- main.js
+++ main.js
@@ -37,3 +37,43 @@ fetch("./services.html")
     .then(data => {
       document.querySelector("services").innerHTML = data;
     });
+
+fetch("./comentarios.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("comentarios").innerHTML = data;
+    });
+
+fetch("./fotos.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("fotos").innerHTML = data;
+    });
+
+fetch("./footer.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("footer").innerHTML = data;
+    });
+
+fetch("./footeramarillo.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("footeramarillo").innerHTML = data;
+    });
+
+fetch("./menu2.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("menu2").innerHTML = data;
+    });
